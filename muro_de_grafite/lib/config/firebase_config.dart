9cb7d0b85bbea4335ccf2bcfb7cb9e95cffabe87
MarsCristian <marsgristian@gmail.com>
2025-08-14 09:54:05 -0300

    Deploy Flutter Web to GitHub Pages

---
+++ muro_de_grafite/lib/config/firebase_config.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseConfig {
+  static const FirebaseOptions options = FirebaseOptions(
+    apiKey: "AIzaSyAAJ9PMXDZKIBtPqziIO4riar3yJEIgHDg",
+    authDomain: "muro-de-grafite.firebaseapp.com",
+    databaseURL: "https://muro-de-grafite-default-rtdb.firebaseio.com",
+    projectId: "muro-de-grafite",
+    storageBucket: "muro-de-grafite.firebasestorage.app",
+    messagingSenderId: "1072514788942",
+    appId: "1:1072514788942:web:85945ff8cbf07b8eed5d1f",
+    measurementId: "G-9B0MES5RWH",
+  );
+}
